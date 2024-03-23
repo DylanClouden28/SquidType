926b6ba4073910d9e047093ba2e3b6098ff375d9
chris <chris@debian.local>
2024-03-23 19:14:37 -0400

    database connection

--- backend/main.go
+++ backend/main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	//"net/http"
+	"context"
 	"github.com/gin-gonic/gin"
 	"sluggers/controller"
+
+	//"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var client *mongo.Client
+
+func collection(collection string) *mongo.Collection {
+	return client.Database("whateversFine").Collection(collection)
+}
 
 func main() {
 	r := gin.Default()
-
-	/*r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, Christopher Potts")
-	})*/
-
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
 	controller.Route(r)
 	r.Run(":8000")
 }
