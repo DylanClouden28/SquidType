d2b2ba246e3c373e8344aa956d9050ed9e2a2eae
chris <chris@debian.local>
2024-03-23 20:54:58 -0400

    added user login and registering

--- backend/controller/controller.go
+++ backend/controller/controller.go
@@ -1,9 +1,17 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"encoding/binary"
+	"encoding/hex"
 	"sluggers/database"
 	"sluggers/models"
+
+	"crypto/sha256"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+	"math/rand"
 )
 
 func Route(router *gin.Engine) {
@@ -15,16 +23,64 @@ func Route(router *gin.Engine) {
 }
 
 func login(c *gin.Context) {
+	var userLogin models.User
+	userCollection := database.Collection("users")
+
+	err := c.BindJSON(&userLogin)
+	if err != nil {
+		c.JSON(400, "Bad JSON")
+		return
+	}
+	filter := bson.D{{"username", userLogin.Username}}
+	var loginAs models.User
+	user_err := userCollection.FindOne(context.TODO(), filter).Decode(*&loginAs)
+	if user_err != nil {
+		c.JSON(400, "No such user")
+		return
+	}
+	success := bcrypt.CompareHashAndPassword([]byte(loginAs.Password), []byte(userLogin.Password))
+	if success != nil {
+		c.JSON(400, "Incorrect passowrd")
+		return
+	}
+	// get hash object
+	hash := sha256.New()
+	// get random int
+	unhashed := rand.Int()
+	// make 8 byte slice and put the int into it
+	bytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytes, uint64(unhashed))
+	// write those bytes to the hash and output it
+	hash.Write(bytes)
+	hashBytes := hash.Sum(nil)
+	// turn the hash into hex
+	hashString := hex.EncodeToString(hashBytes)
+	loginAs.Token = hashString
+	c.SetCookie("auth", hashString, 2100000000, "/", "localhost:8080", true, true)
 
 }
 
 func register(c *gin.Context) {
 	var newUser models.User
-	userCollection := database.Collection("collection")
+	userCollection := database.Collection("users")
 
 	err := c.BindJSON(&newUser)
 	if err != nil {
 		c.JSON(400, "Bad JSON")
+		return
 	}
 
+	hash, hash_err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	if hash_err != nil {
+		c.JSON(400, "Error hashing password(?)")
+		return
+	}
+
+	newUser.Password = string(hash)
+
+	_, ins_err := userCollection.InsertOne(context.TODO(), newUser)
+	if ins_err != nil {
+		c.JSON(400, "Error creating user, try a different username")
+		return
+	}
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,19 +1,5 @@
 package models
 
-import (
-	"encoding/json"
-)
-
-func UnmarshalUserJSON(data []byte) (User, error) {
-	var r User
-	err := json.Unmarshal(data, &r)
-	return r, err
-}
-
-func (r *User) MarshalJSON() ([]byte, error) {
-	return json.Marshal(r)
-}
-
 type User struct {
 	Username string `bson:"username" json:"username"`
 	Password string `bson:"password" json:"password"`
