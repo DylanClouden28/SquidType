0de4e6e784007a946c3fe4345eff7dac9f48f552
chris <chris@debian.local>
2024-03-23 21:47:41 -0400

    allow cors

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	//"net/http"
-	"sluggers/controller"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"sluggers/controller"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
 	controller.Route(r)
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://localhost"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	r.Run(":8000")
 }
