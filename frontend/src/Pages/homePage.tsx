d3264645cd1a0955e0bd0fd7e711688fa746a5bc
Dylan Clouden <dclouden22@gmail.com>
2024-04-18 03:21:53 -0400

    Added mock type racer and progess bars

--- frontend/src/Pages/homePage.tsx
+++ frontend/src/Pages/homePage.tsx
@@ -2,6 +2,7 @@ import sendIcon from '../assets/send-svgrepo-com.svg'
 import LiterallyHim from '../assets/LiterallyHim.jpg'
 import EmojiPicker from 'emoji-picker-react';
 import React, { useEffect, useState } from 'react';
+import GameView from '../components/GameView';
 import { useNavigate } from 'react-router-dom';
 
 import Modal from '../components/Modal'
@@ -220,9 +221,10 @@ function home(){
     //       }
     // }
 
+
     return(
         <div className="p-4 bg-base-300 min-h-screen" data-theme="night">
-            <div className="navbar bg-base-100 rounded-box shadow-xl mb-10 lg:mb-20 p-4">
+            <div className="navbar bg-base-100 rounded-box shadow-xl mb-10 p-4">
             <div className="navbar-start">
                 <img className="p-0 w-10 h-10" src={LiterallyHim} />
             </div>
@@ -241,70 +243,78 @@ function home(){
                 {/* <button className='btn btn-neutral' onClick={openModal}>Open Modal</button> */}
             </div>
             </div>
-            <div className="grid justify-items-center">
 
-                {isHelp && 
-                <div role="alert" className="alert alert-info max-w-2xl mb-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span>Create messages below, react to messages by clicking on message and choosing reaction! Hover over reaction to see user!</span>
-                    <div>
-                        <button onClick={() => {setIsHelp(false)}} className="btn btn-ghost btn-sm">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                        </button>
-                    </div>
+            <div className='flex'>
+                <div className='w-2/3'>
+                    <GameView />
                 </div>
-                }
 
-                <div className="card w-5/6 xl:w-6/12 2xl:w-4/12 shadow-2xl bg-base-200">
-                    <div className={isHelp ? "chatBox p-2 h-[20rem] 2xl:h-[22rem] overflow-auto overflow-x-hidden" : "chatBox p-2 h-[25rem] lg:h-[28rem] overflow-auto overflow-x-hidden"}>
-                            { messages.length > 0 && messages.map(message =>
-                            <div className={username === message?.user ? "chat chat-end" : "chat chat-start"}>
-                                <div className="chat-image avatar">
-                                    <div className="w-10 rounded-full">
-                                    <img alt="Tailwind CSS chat bubble component" src={username === message?.user ? getImageSrc(): `http://localhost:8000/public/images/${message?.user}.png`} />
-                                    </div>
-                                </div>
-                                <div className={username === message?.user ? "chat-header mr-2" : "chat-header ml-2"}>
-                                {message?.user}
-                                <time className="text-xs opacity-50 mx-1">{new Date(message?.date)?.toLocaleString("en-US", {hour: "2-digit", minute: "2-digit"})}</time>
-                                </div>
-                                <div className="chat-bubble hover:bg-base-100" id={message.uuid} role="button" onClick={() => {
-                                    setEmojiDropDown(!isEmojiDropDown);
-                                    setCurrentMessage(message?.uuid);
-                                    }}>
-                                        <div className='tooltip' data-tip="Click to add Emoji">
-                                        {message?.content}
-                                        </div>
-                                    </div>
-                                <div className="chat-footer">
-                                    {message?.reaction && message?.reaction !== null && message.reaction.map(reaction => 
-                                    <div className="badge badge-neutral mx-1 mt-1 tooltip" data-tip={reaction?.username}>
-                                        <span >{reaction?.emoji}</span>
-                                    </div>
-                                    )}
-                                </div>
-                            </div>    
-                            )}
-                    </div>
-
-                    <div className="card-body p-2">
-                        <div className="flex">
-                            <input className="input max-sm:input-sm input-bordered flex-grow mx-2" placeholder="Type here" value={messageInput} type="text" onChange={e => {setMessageInput(e.target.value)}}/>
-                            <img onClick={() => {sendMessage(messageInput)}} className="btn max-sm:btn-sm btn-accent p-2" src={sendIcon}></img>
+                <div className="grid justify-items-center w-1/3">
+                    {isHelp && 
+                    <div role="alert" className="alert alert-info max-w-2xl mb-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <span>Create messages below, react to messages by clicking on message and choosing reaction! Hover over reaction to see user!</span>
+                        <div>
+                            <button onClick={() => {setIsHelp(false)}} className="btn btn-ghost btn-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                            </button>
                         </div>
                     </div>
-                    <div className="dropdown dropdown-top dropdown-end">
-                            <div id="card-body" className=''> 
-                                {isEmojiDropDown &&
-                                    <ul className="dropdown-content shadow-xl z-[50] !visible !opacity-100">
-                                        <li><EmojiPicker onEmojiClick={handleEmojiSelect} open={true}/></li>
-                                    </ul>
-                                }
+                    }
+
+                    <div className="card w-5/6 shadow-2xl bg-base-200">
+                        <div className={isHelp ? "chatBox p-2 h-[20rem] 2xl:h-[22rem] overflow-auto overflow-x-hidden" : "chatBox p-2 h-[25rem] lg:h-[28rem] 2xl:h-[36rem] overflow-auto overflow-x-hidden"}>
+                                { messages.length > 0 && messages.map(message =>
+                                <div className={username === message?.user ? "chat chat-end" : "chat chat-start"}>
+                                    <div className="chat-image avatar">
+                                        <div className="w-10 rounded-full">
+                                        <img alt="Tailwind CSS chat bubble component" src={username === message?.user ? getImageSrc(): `http://localhost:8000/public/images/${message?.user}.png`} />
+                                        </div>
+                                    </div>
+                                    <div className={username === message?.user ? "chat-header mr-2" : "chat-header ml-2"}>
+                                    {message?.user}
+                                    <time className="text-xs opacity-50 mx-1">{new Date(message?.date)?.toLocaleString("en-US", {hour: "2-digit", minute: "2-digit"})}</time>
+                                    </div>
+                                    <div className="chat-bubble hover:bg-base-100" id={message.uuid} role="button" onClick={() => {
+                                        setEmojiDropDown(!isEmojiDropDown);
+                                        setCurrentMessage(message?.uuid);
+                                        }}>
+                                            <div className='tooltip' data-tip="Click to add Emoji">
+                                            {message?.content}
+                                            </div>
+                                        </div>
+                                    <div className="chat-footer">
+                                        {message?.reaction && message?.reaction !== null && message.reaction.map(reaction => 
+                                        <div className="badge badge-neutral mx-1 mt-1 tooltip" data-tip={reaction?.username}>
+                                            <span >{reaction?.emoji}</span>
+                                        </div>
+                                        )}
+                                    </div>
+                                </div>    
+                                )}
+                        </div>
+
+                        <div className="card-body p-2">
+                            <div className="flex">
+                                <input className="input max-sm:input-sm input-bordered flex-grow mx-2" placeholder="Type here" value={messageInput} type="text" onChange={e => {setMessageInput(e.target.value)}}/>
+                                <img onClick={() => {sendMessage(messageInput)}} className="btn max-sm:btn-sm btn-accent p-2" src={sendIcon}></img>
                             </div>
+                        </div>
+                        <div className="dropdown dropdown-top dropdown-end">
+                                <div id="card-body" className=''> 
+                                    {isEmojiDropDown &&
+                                        <ul className="dropdown-content shadow-xl z-[50] !visible !opacity-100">
+                                            <li><EmojiPicker onEmojiClick={handleEmojiSelect} open={true}/></li>
+                                        </ul>
+                                    }
+                                </div>
+                        </div>
                     </div>
                 </div>
+                </div>
+
             </div>
-            </div>
+
     )
 }
 export default home;
---
+++ frontend/src/components/GameView.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react"
+
+
+const GameView = () => {
+    
+    const mockUsers = ['User1', "Dylan", "Chris", "Steve", "Steve", "Steve", "Steve"]
+
+    const [typeInput, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const testTypingText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+    const comparisonText = () =>{
+        let text = [];
+        for (let i =0; i < testTypingText.length; i++){
+            const truthChar = testTypingText[i];
+            let acutalChar = undefined;
+
+            if (typeInput.length > i){
+                acutalChar = typeInput[i];
+                console.log(typeInput.length, truthChar, acutalChar)
+            }
+
+            if (acutalChar === undefined){
+                // console.log("Undefined")
+                text.push(<span className="text-base-content">{truthChar}</span>);
+            }
+            else if(truthChar !== acutalChar){
+                console.log("Error")
+                text.push(<span className="text-error">{truthChar}</span>);
+            }
+            else{
+                console.log("Success")
+                text.push(<span className="text-success">{truthChar}</span>);
+            }
+        }
+        // console.log(text)
+        return text;
+    }   
+
+    return(
+        <div>
+        <div className="flex flex-col w-full justify-center items-center gap-y-6 h-96 overflow-auto">
+            {mockUsers.map(user => 
+                <div id={`progess_${user}`} className="flex flex-row bg-base-100 p-2 card w-5/6">
+                    <div className="avatar flex flex-col h-16 w-16">
+                        <div className="rounded-full">
+                        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-center items-center mx-2 w-full">
+                        {user}
+                        <progress className="progress progress-success w-full h-10" value="60" max="100">asdas</progress>
+                    </div>
+                </div>
+
+            )}
+        </div>
+
+        <div id="typing" className="card bg-base-100 p-4 mt-4 mx-10 h-48">
+            <div className="p-4">
+                <p>
+                {comparisonText()}
+                </p>
+            </div>
+            <input value={typeInput} onChange={handleInputChange} placeholder="Start typing here" className="input input-md input-bordered"></input>
+        </div>
+
+        </div>
+    )
+}
+
+export default GameView
