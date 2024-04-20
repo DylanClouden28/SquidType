cd321de2ccee3fbb7a823fcbcae8875629e2e3eb
chris <chris@debian.local>
2024-04-19 21:36:55 -0400

    futher work on manager

--- backend/game/manager.go
+++ backend/game/manager.go
@@ -1,7 +1,9 @@
 package game
 
 import (
+	"math/rand"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -9,6 +11,7 @@ type player struct {
 	Username          string
 	IsDead            bool
 	CurrentPercentage float64
+	IsReady           bool
 }
 
 const (
@@ -24,13 +27,20 @@ type gameState struct {
 	CurrentLight     int
 	CurrentLightEnds time.Time
 	TargetMessage    string
+	Lock             sync.Mutex
+	Cond             *sync.Cond
 }
 
 type gameUpdate struct {
-	Players       *[]player `json:"players"`
-	CurrentLight  int       `json:"currentLight"`
-	TargetMessage string    `json:"targetMessage"`
-	TargetLength  int
+	Players      *[]player `json:"players"`
+	CurrentLight int       `json:"currentLight"`
+	MessageType  string    `json:"MessageType"`
+	GameOver     bool
+}
+
+type roundStart struct {
+	MessageType   string `json:"MessageType"`
+	TargetMessage string `json:"targetMessage"`
 }
 
 var GameState *gameState
@@ -44,9 +54,35 @@ func incomingMessage(mess message, username string) gameUpdate {
 	for i := 0; i < len(*GameState.Players); i++ {
 		if (*GameState.Players)[i].Username == username {
 			if (*GameState.Players)[i].CurrentPercentage < completed {
+				GameState.Lock.Lock()
+				defer GameState.Lock.Unlock()
 				(*GameState.Players)[i].CurrentPercentage = completed
 			}
 		}
 	}
 	return gameUpdate{}
+}
+
+func GameLoop() {
+	// primary game loop
+	// will always be running
+	GameState.Cond = sync.NewCond(&GameState.Lock)
+	for {
+		// locks mutex and waits for players to be in lobby
+		// waiting unlocks mutexx
+		// TODO replace with ready up
+		GameState.Lock.Lock()
+		for len(*GameState.Players) == 0 {
+			GameState.Cond.Wait()
+		}
+		for i := 0; i < len(*GameState.Players); i++ {
+			(*GameState.Players)[i].IsDead = false
+			(*GameState.Players)[i].CurrentPercentage = 0.0
+		}
+		GameState.TargetMessage = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
+		start := roundStart{MessageType: "roundStart", TargetMessage: GameState.TargetMessage}
+
+		GameState.Lock.Unlock()
+	}
+
 }
